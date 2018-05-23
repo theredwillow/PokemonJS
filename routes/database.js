43499129007661fd9f9c10e279d81e8eb6dd98bc
scribo-o <theredwillows@gmail.com>
2018-05-23 18:07:01 -0500

    Created a database and a select route

---
+++ routes/database.js
@@ -0,0 +1,57 @@
+var express = require('express');
+var router = express.Router();
+var sqlite3 = require('sqlite3').verbose();
+
+function dbCall(req, res, next) {
+
+    var db = new sqlite3.Database('./database.db');
+    /*
+    Possible Variables
+        loc: location
+        user: user
+        newLoc: replacement location in update query or new location in insert
+        newUser: replacement username in update query or new user in insert
+        database: locations or users, chosen via loc or user existance
+        queryType: select, insert, update, etc... chosen via route
+    */
+
+    // var queryType = req.url.replace(/(^\/)|(\?.*$)/g,"").toUpperCase();
+
+    if ( !req.query.loc && !req.query.user && !req.query.table ) {
+        req.data = { "error": "No table name provided." };
+        return next();
+    }
+
+    var table = req.query.table || ( req.query.loc ) ? "locations" : "users";
+    var item = req.query.loc || req.query.user;
+
+    var displayData = function(error, rows) {
+        // console.log("rows", rows);
+        if ( rows && rows.length !== 0 )
+            req.data = rows;
+        else
+            req.data = { "error": "No such records found." };
+        return next();
+    };
+
+    var dbRequest = "SELECT * FROM " + table;
+    if (item) {
+        dbRequest += " WHERE name = ?";
+        db.all(dbRequest, item, displayData);
+    }
+    else {
+        db.all(dbRequest, displayData);
+    }
+
+}
+
+function renderData(req, res) {
+    res.send( "poop:" + JSON.stringify(req.data) );
+    db.close();
+}
+
+router.get('/select', dbCall, renderData);
+// router.get('/insert', dbCall, renderData);
+// router.get('/update', dbCall, renderData);
+
+module.exports = router;
